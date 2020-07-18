aa983e43123f8b3ac4fdaa6ddafd69bf484fca50
LuisFernandoSilva <37556159+LuisFernandoSilva@users.noreply.github.com>
2020-07-18 15:56:41 -0300

    tela de ediçao fixa apenas recebendo do banco, layout, add assets

--- lib/pages/edit/reg_product_page.dart
+++ lib/pages/edit/reg_product_page.dart
@@ -19,8 +19,58 @@ class _RegProductPageState extends State<RegProductPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cadastro Produto: ${_product?.name ?? ''}'),
+        title: Text('Cadastro Produto'),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                ),
+                child: AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: FadeInImage(
+                    placeholder: AssetImage('assets/loading.gif'),
+                    image: NetworkImage(_product?.photo ?? ''),
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.photo_camera),
+                    label: Text('Tirar'),
+                  ),
+                  SizedBox(width: 8),
+                  RaisedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.photo_library),
+                    label: Text('Galeria'),
+                  ),
+                ],
+              ),
+              Divider(),
+              Text(
+                'Nome: ${_product?.name ?? ''}',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 8),
+              Text(
+                '${_product?.description ?? ''}',
+                style: TextStyle(fontSize: 12),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
