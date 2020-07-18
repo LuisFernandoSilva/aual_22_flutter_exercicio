efe5d6774699f83e3ad2c2ba2195659c5d7fcaae
LuisFernandoSilva <37556159+LuisFernandoSilva@users.noreply.github.com>
2020-07-18 16:11:54 -0300

    add botoes de salvar e cancelar layout tela edit

--- lib/pages/edit/reg_product_page.dart
+++ lib/pages/edit/reg_product_page.dart
@@ -24,52 +24,84 @@ class _RegProductPageState extends State<RegProductPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white),
+        child: Column(
+          //mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                      ),
+                      child: AspectRatio(
+                        aspectRatio: 16 / 9,
+                        child: FadeInImage(
+                          placeholder: AssetImage('assets/loading.gif'),
+                          image: NetworkImage(_product?.photo ?? ''),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        RaisedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.photo_camera),
+                          label: Text('Tirar'),
+                        ),
+                        SizedBox(width: 8),
+                        RaisedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.photo_library),
+                          label: Text('Galeria'),
+                        ),
+                      ],
+                    ),
+                    Divider(),
+                    Text(
+                      'Nome: ${_product?.name ?? ''}',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      '${_product?.description ?? ''}',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ],
                 ),
-                child: AspectRatio(
-                  aspectRatio: 16 / 9,
-                  child: FadeInImage(
-                    placeholder: AssetImage('assets/loading.gif'),
-                    image: NetworkImage(_product?.photo ?? ''),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 40,
+                  child: OutlineButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Cancelar'),
+                    borderSide: BorderSide(color: Colors.blue),
+                    focusColor: Colors.red,
                   ),
                 ),
-              ),
-              SizedBox(height: 8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton.icon(
+                SizedBox(width: 16),
+                Expanded(
+                  flex: 60,
+                  child: OutlineButton(
+                    child: Text('Salvar'),
                     onPressed: () {},
-                    icon: Icon(Icons.photo_camera),
-                    label: Text('Tirar'),
+                    borderSide: BorderSide(color: Colors.blue),
+                    focusColor: Colors.red,
                   ),
-                  SizedBox(width: 8),
-                  RaisedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.photo_library),
-                    label: Text('Galeria'),
-                  ),
-                ],
-              ),
-              Divider(),
-              Text(
-                'Nome: ${_product?.name ?? ''}',
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(height: 8),
-              Text(
-                '${_product?.description ?? ''}',
-                style: TextStyle(fontSize: 12),
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
